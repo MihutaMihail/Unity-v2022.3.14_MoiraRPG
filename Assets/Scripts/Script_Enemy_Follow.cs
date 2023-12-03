0eee43cb5556f9e31738803e55a2362598c43861
Mihuta Mihail <mihutamihail95@gmail.com>
2023-12-03 16:09:54 +0100

    [ADD] once enemy stops following, returns back to starting position

--- Assets/Scripts/Script_Enemy_Follow.cs
+++ Assets/Scripts/Script_Enemy_Follow.cs
@@ -9,43 +9,88 @@ public class Script_Enemy_Follow : MonoBehaviour
     public Color attackColor;
 
     // Private
-    private Rigidbody2D rb;
+    private const float ReturnThreshold = 0.1f;
+
+    private Rigidbody2D rigidBody;
+    private Vector3 startPosition;
     private Transform player;
+
     private Vector2 movement;
-    private SpriteRenderer spriteRenderer;
+    private Vector2 returnMovement;
     private bool isFollowing = false;
+    private bool isReturningToStartingPosition = false;
 
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
     }
 
     void Update()
     {
-        if (isFollowing)
-        {
-            Vector2 direction = player.position - transform.position;
-            direction.Normalize();
-            movement = direction;
-        }       
+        if (isFollowing) movement = CalculateDirection(player.position);
+        else if (isReturningToStartingPosition) returnMovement = CalculateDirection(startPosition);
     }
-    
+
     private void FixedUpdate()
     {
-        if (isFollowing)
-        {
-            FollowPlayer(movement);
-        }
+        if (isFollowing) FollowPlayer(movement);
+        else if (isReturningToStartingPosition) ReturnToStartingPosition(returnMovement);
     }
+
+    //
+    // Direction
+    //
+
+    private Vector2 CalculateDirection(Vector3 targetPosition)
+    {
+        // Calculate vector pointing from the enemy position to the player position
+        Vector2 direction = targetPosition - transform.position;
+
+        // Normalize vector to get direction from enemy to player
+        direction.Normalize();
+
+        return direction;
+    }
+
+    //
+    // Movement
+    //
 
     private void FollowPlayer(Vector2 direction)
     {
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        // Move gameObject(enemy) towards player
+        rigidBody.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.fixedDeltaTime));
     }
 
+    private void ReturnToStartingPosition(Vector2 returnDirection)
+    {
+        // Move gameObject(enemy) towards starting position
+        rigidBody.MovePosition((Vector2)transform.position + (returnDirection * moveSpeed * Time.fixedDeltaTime));
+
+        // Calculate distance between gameObject and player
+        float distanceToStartingPosition = Vector2.Distance(transform.position, startPosition);
+
+        // Check if distance to starting position from the enemy position is in threshold
+        if (distanceToStartingPosition < ReturnThreshold)
+        {
+            // Stop further movement
+            rigidBody.velocity = Vector2.zero;
+            
+            // Reset enemy to starting position
+            transform.position = startPosition;
+            
+            // Reset flag
+            isReturningToStartingPosition = false;
+        }
+    }
+
+    //
     // Trigger
+    //
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) isFollowing = true;
@@ -53,6 +98,10 @@ public class Script_Enemy_Follow : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) isFollowing = false;
+        if (collision.CompareTag("Player"))
+        {
+            isFollowing = false;
+            isReturningToStartingPosition = true;
+        }
     }
 }
