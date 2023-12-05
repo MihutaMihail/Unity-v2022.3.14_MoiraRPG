8427b99314ad7c5c226d97917b3e3df7078aa900
MIHUTA Mihail <mihail.mihuta@ynov.com>
2023-12-05 16:31:43 +0100

    [ADD] sentence onto dialogue box + animation open/close + add letter by letter for each sentence

--- Assets/Scripts/DialogBox/Dialogue_Manager.cs
+++ Assets/Scripts/DialogBox/Dialogue_Manager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dialogue_Manager : MonoBehaviour
 {
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
     private Queue<string> sentences;
 
     void Start()
@@ -13,6 +19,47 @@ public class Dialogue_Manager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialog()
+    {
+        animator.SetBool("isOpen", false);
     }
 }
