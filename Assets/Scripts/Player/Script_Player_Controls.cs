80941a6336c7c6d88d310c13004758f60bd22e6b
MIHUTA Mihail <mihail.mihuta@ynov.com>
2024-01-15 15:35:27 +0100

    [ADD] Dash IMPORTANT : the dash is executed, but in the wrong direction (to fix)

--- Assets/Scripts/Player/Script_Player_Controls.cs
+++ Assets/Scripts/Player/Script_Player_Controls.cs
@@ -1,12 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class Script_Player_Controls : MonoBehaviour
 {
     public float moveSpeed;
+    public float dashingPower;
+    public float dashingTime;
+    public float dashingCooldown;
 
     private Vector2 movementVector;
     private Rigidbody2D rb;
 
+    private bool canDash = true;
+    private bool isDashing = false;
+    
     void Start()
     {
         Cursor.visible = false;
@@ -16,16 +23,46 @@ public class Script_Player_Controls : MonoBehaviour
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         movementVector = new Vector2(
-            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
         );
 
         movementVector.Normalize();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
     
     void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+        
         rb.velocity = new Vector2(movementVector.x * moveSpeed, movementVector.y * moveSpeed);
+    }
+
+    //
+    // Dash
+    // 
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, transform.localScale.y * dashingPower);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
