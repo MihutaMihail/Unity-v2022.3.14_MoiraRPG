0fef0b3f668237be88cf2979f567b0b92c22fc8a
Mihuta Mihail <mihutamihail95@gmail.com>
2024-01-16 13:03:30 +0100

    [ADD] stamina bar + [ADD] sprint mechanic + IMPORTANT : check docs for known problems to fix

--- Assets/Scripts/Player/Script_Player_Controls.cs
+++ Assets/Scripts/Player/Script_Player_Controls.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Script_Player_Controls : MonoBehaviour
 {
-    public float moveSpeed;
-    public float dashingPower;
-    public float dashingTime;
-    public float dashingCooldown;
+    public float moveSpeed, sprintSpeedMultiplier, sprintCost;
+    public float dashingPower, dashingTime, dashingCost;
+
+    public Image staminaBar;
+    public float stamina, maxStamina;
 
     private Vector2 movementVector;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private bool isSprinting;
 
     private bool canDash = true;
     private bool isDashing = false;
-    
+    private bool isInvincible = false;
+
     void Start()
     {
         Cursor.visible = false;
 
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
-
+    
     void Update()
     {
         if (isDashing)
@@ -35,9 +42,18 @@ public class Script_Player_Controls : MonoBehaviour
 
         movementVector.Normalize();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
         {
             StartCoroutine(Dash());
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }  
+        else
+        {
+            isSprinting = false;
         }
     }
     
@@ -47,8 +63,21 @@ public class Script_Player_Controls : MonoBehaviour
         {
             return;
         }
-        
-        rb.velocity = new Vector2(movementVector.x * moveSpeed, movementVector.y * moveSpeed);
+
+        float currentMoveSpeed = isSprinting ? moveSpeed * sprintSpeedMultiplier : moveSpeed;
+
+        rb.velocity = new Vector2(movementVector.x * currentMoveSpeed, movementVector.y * currentMoveSpeed);
+
+        if (isSprinting)
+        {
+            stamina -= sprintCost * Time.fixedDeltaTime;
+            staminaBar.fillAmount = stamina / maxStamina;
+
+            if (stamina <= 0)
+            {
+                isSprinting = false;
+            }
+        }
     }
 
     //
@@ -57,12 +86,32 @@ public class Script_Player_Controls : MonoBehaviour
 
     private IEnumerator Dash()
     {
+        // Decrease stamina
+        stamina -= dashingCost;
+        if (stamina < 0) stamina = 0;
+        staminaBar.fillAmount = stamina / maxStamina;
+
+        // Temporarily disable dashing
         canDash = false;
+        
         isDashing = true;
+        isInvincible = true;
+
+        // Change color during invincibility state
+        sr.color = Color.black;
+
+        // Apply velocity for the dash in the specified direction
         rb.velocity = new Vector2(movementVector.x * dashingPower, movementVector.y * dashingPower);
+
         yield return new WaitForSeconds(dashingTime);
+
+        // Change color to original after dash ended
+        sr.color = Color.white;
+
         isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
+        isInvincible = false;
+
+        // Enable dashing
         canDash = true;
     }
 }
